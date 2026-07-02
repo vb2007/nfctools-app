ef6df79564ab8d2d459bfe874e79acdf3e5e59b6
VB2007 <admin@vb2007.hu>
2026-07-02 02:10:26 +0200

    fixed fatal import error

--- app/src/main/java/hu/vb2007/nfctool/di/AppModule.kt
+++ app/src/main/java/hu/vb2007/nfctool/di/AppModule.kt
@@ -6,6 +6,9 @@ import hu.vb2007.nfctool.nfc.read.TagReader
 import hu.vb2007.nfctool.nfc.read.TagTypeDetector
 import hu.vb2007.nfctool.nfc.read.UltralightDumpReader
 import hu.vb2007.nfctool.nfc.write.NdefTextWriter
+import hu.vb2007.nfctool.ui.read.ReadViewModel
+import hu.vb2007.nfctool.ui.write.WriteViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -15,4 +18,7 @@ val appModule = module {
     single { TagReader(get(), get(), get()) }
     single { NdefTextWriter() }
     single { NfcController(get(), get(), get()) }
+
+    viewModel { ReadViewModel(get()) }
+    viewModel { WriteViewModel(get()) }
 }

ac50176bdbe03bca8f2ce7eb0be1796fb812bf0c
VB2007 <admin@vb2007.hu>
2026-07-05 17:32:38 +0200

    updated version in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "hu.vb2007.nfctool"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes {
